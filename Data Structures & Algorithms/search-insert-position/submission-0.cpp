65470fee30570053692878af48afb6e416a09faf
Nguyễn Tứ Cường <tucuongbrt@gmail.com>
2026-09-10 23:13:49 +0700

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int l = 0, r =nums.size()-1;
+        int res =nums.size();
+        while(l<=r){
+
+            int m = l +(r-l)/2;
+            if(nums[m] < target){
+                l = m+1;
+            } else if (nums[m] > target){
+                res = m ;
+                r = m-1;
+            } else {
+                return m;
+            }
+        }
+        return res;
+
+    }
+};
